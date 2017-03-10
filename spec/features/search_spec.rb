e3fb2df74bb838ed0f565ef7d8431f03e292fdb3
picholasnaridon <paridonn1@gmail.com>
2017-03-10 14:40:31 -0500

    hound 3

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 feature "Search for item using search bar" do
-  let!(:user) { FactoryGirl.create(:user) }
-  let!(:item_1) { FactoryGirl.create(:item, user: user) }
-  let!(:item_2) { FactoryGirl.create(:item, user: user) }
+    let!(:user) { FactoryGirl.create(:user) }
+    let!(:item_1) { FactoryGirl.create(:item, user: user) }
+    let!(:item_2) { FactoryGirl.create(:item, user: user) }
 
   scenario 'user searches for an item that does exist ' do
     visit root_path
@@ -28,7 +28,7 @@ feature "Search for item using search bar" do
   scenario 'user searches for an item that does not exist ' do
     visit root_path
     click_link 'Items'
-    fill_in 'term', with: item_1.name
+    fill_in 'term', with: item_2.name
     click_button 'Search'
 
     expect(page).to_not have_content(item_1.name)
