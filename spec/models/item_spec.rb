e6b364c464c90458d81ee600dbf2cd2113995ed3
servbotsu <tealsasuke@aol.com>
2017-03-15 12:18:10 -0400

    Added sad paths for tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,4 +5,8 @@ RSpec.describe Item, type: :item do
   it { should have_valid(:description).when('Test', 'hello') }
   it { should have_valid(:youtube_url).when('https://www.test.com') }
   it { should have_valid(:purchase_url).when('https://www.youtube.com/watch?v=j4z3xv2l9_k') }
+
+  it { should_not have_valid(:name).when(nil) }
+  it { should_not have_valid(:youtube_url).when('youtube.com') }
+  it { should_not have_valid(:youtube_url).when('test.com') }
 end
