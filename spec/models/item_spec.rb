fdbf8bbbdc2dbd4b6db62a0fcbaae371653a7ea2
servbotsu <tealsasuke@aol.com>
2017-03-15 11:44:35 -0400

    Swapped tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe Item, type: :item do
 
   it { should have_valid(:description).when('Test', 'hello') }
-  it { should have_valid(:youtube_url).when('https://www.test.com') }
-  it { should have_valid(:purchase_url).when('https://www.youtube.com/watch?v=j4z3xv2l9_k') }
+  it { should have_valid(:youtube_url).when('https://www.youtube.com/watch?v=j4z3xv2l9_k') }
+  it { should have_valid(:purchase_url).when('https://www.test.com') }
 end
