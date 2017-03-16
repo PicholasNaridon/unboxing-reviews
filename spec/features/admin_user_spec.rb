921cec1c8378cb919852520e8d7293924ba92569
picholasnaridon <paridonn1@gmail.com>
2017-03-16 09:10:09 -0400

    hound fixes

--- spec/features/admin_user_spec.rb
+++ spec/features/admin_user_spec.rb
@@ -1,19 +1,8 @@
 require 'rails_helper'
 
 feature "admin sees all users" do
-  let!(:user)  { User.create(
-    email: 'user@user.com',
-    username: 'username',
-    password: 'password',
-    password_confirmation: 'password'
-    )}
-  let!(:admin)  { User.create(
-    email: 'user@user.com',
-    username: 'username',
-    password: 'password',
-    password_confirmation: 'password',
-    admin: true
-    )}
+  let!(:user)  { User.create(email: 'user@user.com', username: 'username', password: 'password', password_confirmation: 'password')}
+  let!(:admin)  { User.create(email: 'user@user.com', username: 'username', password: 'password', password_confirmation: 'password', admin: true)}
 
   scenario 'admin visits all users link and sees all users' do
     visit root_path
